01c53707aa0121df086f685bba90483252f18190
Gabriel Henrique Silva <gabs@192.168.1.11>
2020-12-28 12:16:56 -0300

    Generating test to member controller

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -1,26 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe MembersController, type: :controller do
+  include Devise::Test::ControllerHelpers
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  before(:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @current_user = FactoryBot.create(:user)
+    sign_in @current_user
+  end
+
+  describe "POST #create" do
+    before(:each) do
+      @campaign = create(:campaign, user: @current_user)
+      request.env["HTTP_ACCEPT"] = 'application/json'
+    end
+
+    it 'Create member with right attributes' do
+      member = create(:member, campaign: @campaign)
+      expect(Member.last.name).to eql(member.name)
+      expect(Member.last.email).to eql(member.email)
+      expect(Member.last.campaign_id).to eql(@campaign.id)
     end
   end
 
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
+  describe "PUT #update" do
+    before(:each) do
+      @campaign = create(:campaign, user: @current_user)
+      @new_member_attributes = attributes_for(:member)
+      request.env["HTTP_ACCEPT"] = 'application/json'
+    end
+
+    it 'Create member with right attributes' do
+      member = create(:member, campaign: @campaign)
+      put :update, params: {id: member.id, member: @new_member_attributes}
+      expect(true).to eql(JSON(response.body))
     end
   end
 
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
+  describe "DELETE #destroy" do
+    before(:each) do
+      @campaign = create(:campaign, user: @current_user)
+      request.env['HTTP_ACCEPT'] = 'application/json'
     end
+
+    it 'return json true' do
+      member = create(:member, campaign: @campaign)
+      delete :destroy, params: {id: member.id}
+      expect(true).to eql(JSON(response.body))
+    end
+
   end
+  
 
 end
