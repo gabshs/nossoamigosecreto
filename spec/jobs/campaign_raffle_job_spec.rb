fc0e928cdaefc9f72f92533d0e2c3a2f333d3ccf
Gabriel Henrique Silva <gabs@192.168.1.11>
2020-12-28 17:46:07 -0300

    Creating test job

--- spec/jobs/campaign_raffle_job_spec.rb
+++ spec/jobs/campaign_raffle_job_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe CampaignRaffleJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    ActiveJob::Base.queue_adapter = :test
+    @campaign = create(:campaign, status: :pending)
+    create(:member, campaign: @campaign)
+    create(:member, campaign: @campaign)
+    create(:member, campaign: @campaign)
+  end
+
+  describe "#perform" do
+    it "to have enqueued" do
+      expect{CampaignRaffleJob.perform_later(@campaign)}.to have_enqueued_job
+    end
+  end
 end
